81f81a417318757e9ccfcee0865f4e2333d83212
Sara Fritz <31226863+sarafritz88@users.noreply.github.com>
2018-04-25 12:27:20 -0500

    Coming Soon Page

---
+++ react-ui/src/constants/routes.js
@@ -0,0 +1,11 @@
+export const SIGN_UP = '/signup';
+export const SIGN_IN = '/signin';
+export const LANDING = '/landing';
+export const HOME = '/home';
+export const ACCOUNT = '/account';
+export const PASSWORD_FORGET = '/pw-forget';
+export const INVITE ='/invite';
+export const STATS ='/Stat';
+export const SETTINGS = '/Settings';
+export const PASSWORD = '/Password';
+export const COMINGSOON ='/'; // THIS IS WHAT YOU CHANGE TO GET THE LANDING PAGE BACK INSTEAD OF THE COMING SOON PAGE. ALSO NAVIGATION.JS, uncomment 46 and 47 -sf
