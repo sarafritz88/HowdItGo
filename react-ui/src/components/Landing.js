d2b66a406328dcbc20aca089fdaed009222629be
Chris Atoki <christopher.atoki@yahoo.com>
2018-04-25 13:29:08 -0400

    fixed landing page merge error

--- react-ui/src/components/Landing.js
+++ react-ui/src/components/Landing.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './landing.css';
 import * as routes from '../constants/routes';
 import { Link } from 'react-router-dom';
-<<<<<<< HEAD
 import 'font-awesome/css/font-awesome.min.css';
 
 
@@ -41,21 +40,6 @@ const LandingPage = () =>
             </div>
         </div>
         </div>
-=======
-
-const LandingPage = () =>
-    <div>
-        <div class="top">
-        <h1>Help Your Business Stand Out</h1>
-            <h2>Let Your Customers Share Their Experience</h2>
-            <button class="b1"><span><Link to={routes.SIGN_UP}> Get Started</Link></span></button>
-        </div>
-        <div class="lower">
-            <h3> Gaining customer feedback and online review management has never been easier.  </h3>
-            <h4>Easily drive reviews of your business and improve customer service with HowdItGo Customer Interaction Software. </h4>
-        </div>
-    </div>
->>>>>>> 6331c7791f598e6232617fd6d882d8b35854b66a
 
 
 export default LandingPage;
