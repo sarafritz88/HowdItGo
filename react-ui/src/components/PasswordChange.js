1f8bca32c83c8c0581c508250a311d8962882996
Chris Atoki <christopher.atoki@yahoo.com>
2018-05-25 17:32:49 -0400

    Password change works

--- react-ui/src/components/PasswordChange.js
+++ react-ui/src/components/PasswordChange.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react';
+import React, {
+  Component
+} from 'react';
 import axios from 'axios';
 
 export default class PasswordChange extends Component {
@@ -21,18 +23,24 @@ export default class PasswordChange extends Component {
   }
 
   handleChange = event => {
-    this.setState({ [event.target.name]: event.target.value });
+    this.setState({
+      [event.target.name]: event.target.value
+    });
   };
 
   onSubmit = event => {
     event.preventDefault();
     if (this.state.passwordOne !== this.state.passwordTwo) {
       alert("Passwords don't match!");
-      this.setState({ passwordOne: '', passwordTwo: '', username: '' });
+      this.setState({
+        passwordOne: '',
+        passwordTwo: '',
+        username: ''
+      });
       return;
     } else {
       axios
-        .post('update-user', {
+        .post('/update-user', {
           username: this.state.username,
           password: this.state.passwordOne
         })
@@ -42,49 +50,75 @@ export default class PasswordChange extends Component {
   };
 
   render() {
-    const { passwordOne, passwordTwo, username } = this.state;
+    const {
+      passwordOne,
+      passwordTwo,
+      username
+    } = this.state;
 
     const isInvalid =
       passwordOne === '' || passwordTwo === '' || username === '';
 
-    return (
-      <div className="form">
-        <form onSubmit={this.onSubmit}>
-          <div>
-            <div className="label">Username:</div>
-            <input
-              value={username}
-              onChange={this.handleChange}
-              type="text"
-              name="username"
-              placeholder="Username"
-            />
-          </div>
-          <div>
-            <div className="label">New Password:</div>
-            <input
-              value={passwordOne}
-              onChange={this.handleChange}
-              type="password"
-              name="passwordOne"
-              placeholder="New Password"
-            />
-          </div>
-          <div>
-            <div className="label">Confirm Password:</div>
-            <input
-              value={passwordTwo}
-              onChange={this.handleChange}
-              type="password"
-              name="passwordTwo"
-              placeholder="Confirm Password"
-            />
-          </div>
-          <button disabled={isInvalid} type="submit">
-            Submit
-          </button>
-        </form>
-      </div>
+    return ( <
+      div className = "form" >
+      <
+      form onSubmit = {
+        this.onSubmit
+      } >
+      <
+      div >
+      <
+      div className = "label" > Username: < /div> <
+      input value = {
+        username
+      }
+      onChange = {
+        this.handleChange
+      }
+      type = "text"
+      name = "username"
+      placeholder = "Username" /
+      >
+      <
+      /div> <
+      div >
+      <
+      div className = "label" > New Password: < /div> <
+      input value = {
+        passwordOne
+      }
+      onChange = {
+        this.handleChange
+      }
+      type = "password"
+      name = "passwordOne"
+      placeholder = "New Password" /
+      >
+      <
+      /div> <
+      div >
+      <
+      div className = "label" > Confirm Password: < /div> <
+      input value = {
+        passwordTwo
+      }
+      onChange = {
+        this.handleChange
+      }
+      type = "password"
+      name = "passwordTwo"
+      placeholder = "Confirm Password" /
+      >
+      <
+      /div> <
+      button disabled = {
+        isInvalid
+      }
+      type = "submit" >
+      Submit <
+      /button> <
+      /form> <
+      /div>
     );
   }
 }
