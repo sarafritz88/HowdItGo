aca8967608a5c9a4050e50a763cd7363f448e648
Sara Fritz <krumsara@gmail.com>
2019-03-19 12:58:26 -0500

    css change

--- react-ui/src/components/Navigation.js
+++ react-ui/src/components/Navigation.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import {
   Link
 } from 'react-router-dom';
-import './navigation.css';
 import 'font-awesome/css/font-awesome.min.css';
 
 import SignOutButton from './SignOut';
